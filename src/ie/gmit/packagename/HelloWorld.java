b4368d04f46d2e479d25be534829806cfa0385cf
AliciaMcHugh <g00370183@gmit.ie>
2020-01-20 17:38:19 +0000

    ForLoop

--- src/ie/gmit/packagename/HelloWorld.java
+++ src/ie/gmit/packagename/HelloWorld.java
@@ -3,8 +3,11 @@ package ie.gmit.packagename;
 class HelloWorld {
 
     public static void main(String[] args) {
-        // Print Hello World! to the console.
+        // Print Hello World! to the console. 
+        for (int i = 0; i < 10; i++) {
         System.out.println("Hello World!");
+        }
+       
     }
 
 }
